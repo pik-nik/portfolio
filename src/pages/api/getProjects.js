32ae5386e7890c9b14c7ae9a2a5619f211a37dbd
Nikki Pham <nikki.piknik@gmail.com>
2023-05-02 15:47:35 +1000

    trying to deploy

--- src/pages/api/getProjects.js
+++ src/pages/api/getProjects.js
@@ -10,5 +10,3 @@ export default async function handler(req, res) {
     const projects = await client.fetch(query)
     res.status(200).json({ projects })
 }
-
-// check this when projects are added
