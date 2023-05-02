9218c44b42f503337dfb447042b5e08d5dceabf0
Nikki Pham <nikki.piknik@gmail.com>
2023-05-02 23:53:37 +1000

    hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import BackgroundOvals from "./BackgroundOvals"
+import BackgroundCircles from "./BackgroundCircles"
 import Image from "next/image"
 import { urlFor } from "../../sanity"
 import NavArrow from "./NavArrow"
@@ -6,20 +6,20 @@ import Link from "next/link"
 
 function Hero({ pageInfo }) {
     return (
-        <div className="relative align-middle flex flex-col md:flex-row  justify-evenly items-center text-center md:text-left h-screen">
+        <div className="relative align-middle flex flex-col md:flex-row justify-center space-y-5 items-center text-center md:text-left min-h-screen max-h-fit">
             <div className="">
-                <h2 className="text-4xl pb-2">Hello, my name is</h2>
+                <h2 className="text-2xl md:text-4xl pb-2">Hello, my name is</h2>
                 <h1 className="md:text-7xl sm:text-6xl text-5xl text-primary dark:text-primary_dark font-bold pb-2">
                     {pageInfo?.name}
                 </h1>
 
-                <h2 className="md:text-2xl sm:text-xl text-lg uppercase font-mono tracking-[4px] pb-2">
+                <h2 className="md:text-2xl sm:text-xl text-md uppercase font-mono tracking-[4px] pb-2">
                     {pageInfo?.role}
                 </h2>
-                <h2 className="md:text-2xl sm:text-xl text-lg uppercase font-mono tracking-[4px] pb-2">
+                <h2 className="md:text-2xl sm:text-xl text-md uppercase font-mono tracking-[4px] pb-2">
                     based in {pageInfo?.location}
                 </h2>
-                <h3 className="font-sans text-l pb-2">
+                <h3 className="font-sans text-sm md:text-lg pb-2">
                     {pageInfo?.introDescription}
                 </h3>
                 <Link
@@ -27,20 +27,20 @@ function Hero({ pageInfo }) {
                     target="_blank"
                     rel="noreferrer noopener"
                 >
-                    <button className="button text-lg">
+                    <button className="button text-md md:text-lg">
                         Check out my resume here
                     </button>
                 </Link>
             </div>
-            <div className="ml-10">
-                <BackgroundOvals />
-                <div className="rounded-full h-96 w-96 mx-auto object-cover">
+            <div className="m-0 md:ml-10">
+                <BackgroundCircles />
+                <div className="rounded-full h-48 w-48 md:h-96 md:w-96 mx-auto object-cover block">
                     <Image
                         src={urlFor(pageInfo?.heroImage).url()}
                         alt={`profile pic of ${pageInfo?.name}`}
                         width="390"
                         height="390"
-                        className="rounded-full object-contain h-[390px] w-[390px] z-100"
+                        className="rounded-full object-contain h-[250px] w-[250px] md:h-[390px] md:w-[390px] z-100"
                         priority
                     />
                 </div>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -11,14 +11,22 @@ function ProjectCard({ project, selected }) {
                 selected ? "opacity-100" : "opacity-40"
             }`}
         >
-            <Link href={project?.linkToGithub}>
+            <Link
+                href={project?.linkToGithub}
+                target="_blank"
+                rel="noreferrer noopener"
+            >
                 <IoLogoGithub
                     className="absolute top-5 right-5 cursor-pointer text-primary dark:text-primary_dark hover:text-secondary hover:dark:text-secondary_dark"
                     size={30}
                 />
             </Link>
             {project.linkToBuild ? (
-                <Link href={project.linkToBuild}>
+                <Link
+                    href={project.linkToBuild}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
                     <GoLinkExternal
                         className="absolute top-5 right-14 cursor-pointer text-primary dark:text-primary_dark hover:text-secondary hover:dark:text-secondary_dark"
                         size={30}
